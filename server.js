fb63bb3bde1f365395efe88543e4e47a5defce02
Elena Agapova <elena.agapova@rohlik.cz>
2021-04-18 22:14:47 +0200

    setting things up

--- server.js
+++ server.js
@@ -1,17 +1,58 @@
+/** imports **/
 const express = require('express')
 const app = express()
-const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const morgan = require('morgan');
 
-app.use(cors())
-app.use(express.static('public'))
+/** middleware **/
+app.use(morgan('combined'))
+app.use('/public', express.static(`${process.cwd()}/public`));
+app.use('/api', bodyParser.urlencoded({extended: false})); 
+
+/** homepage **/
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+/** db setup **/
+mongoose
+     .connect( process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+     .then(() => console.log( 'Database Connected' ))
+     .catch(err => console.log( err ));
 
 
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  } 
+});
 
+const User = mongoose.model('User', userSchema);
+
+/** db methods **/
+const createUser = (username, done) => {
+  var user = new User({username: username});
+  user.save((err, data)=> {
+    if (err) return console.error(err);
+    done(null, data);
+  });
+};
+
+/** routing **/
+/* create a new user POST username: /api/new-user */ 
+app.post('/api/new-user', (req, res) => {
+createUser(req.body.username, (err, data) => {
+  if (err) return res.json('oops something went wrong').send;
+  return res.json({username: data.username, _id: data._id}).send();
+});
+})
+
+// app.get('/api/users', (req, res) => {
+  
+// });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)

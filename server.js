daa70f6d23a835f873fe32310ff7e0d13614215b
Elena Agapova <elena.agapova@rohlik.cz>
2021-04-20 21:23:10 +0200

    exercise creation done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const mongoose = require('mongoose');
+const mongoose = require('mongoose').set('debug', true);
 const bodyParser = require('body-parser')
 const morgan = require('morgan');
 
@@ -24,13 +24,41 @@ mongoose
 
 
 const userSchema = new mongoose.Schema({
+  // _id: mongoose.Schema.Types.ObjectId,
   username: {
     type: String,
     required: true
   } 
 });
 
+const exerciseSchema = new mongoose.Schema({
+  user_id: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  duration:{
+    type: Number,
+    required: true
+  },
+  date: Date
+});
+
 const User = mongoose.model('User', userSchema);
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+/** helpers **/
+const getDate = () => { // yyyy-mm-dd
+  let date = new Date();
+  let y = date.getFullYear().toString();
+  let m = date.getMonth() + 1;
+  if (m < 10) m = '0' + m.toString();
+  let d = date.getDate().toString();
+  return y + '-' + m + '-' + d;
+}
 
 
 /** db methods **/
@@ -47,6 +75,21 @@ const getAllUsers = (filter, done)  => {
     if (err) console.error(err);
     done(null, data)
   })
+};
+
+const findUser = (id, done) => {
+  User.findOne({_id: id}, (err, data) => {
+    if (err) console.error(err);
+    done(null, data);
+  });
+};
+
+const createExercise = (user, e, done) => {
+  var exercise = new Exercise({user_id: e.userId, description: e.description, duration: e.duration, date: e.date});
+  exercise.save( (err, data) => {
+    if (err) console.error(err);
+    done(null, data);
+  });
 };
 
 /** routing **/
@@ -68,6 +111,43 @@ app.get('/api/users', (req, res) => {
   })
 });
 
+// You can POST to /api/exercise/add with form data userId, description, duration, and optionally date. 
+// If no date is supplied, the current date will be used. 
+// The response returned will be the user object with the exercise fields added.
+app.post('/api/exercise/add', (req, res) => { // /api/users/:_id/exercises
+  if (!req.body.userId || !req.body.description || !req.body.duration) res.send('some of required data are missing');
+  if (!req.body.date) req.body.date = getDate();
+
+  findUser(req.body.userId, (err, data) => { 
+    if (err) return res.send('the user does not exist');
+    var user = data;
+    createExercise(user, req.body, (err, data) => {
+      if (err) res.send('could not save the exercise');
+      var e = data;
+      res.send({_id: user._id, username: user.username, exercise: {description: e.description, duration: e.duration, date: e.date}}); // TODO: better date formatting
+    });
+  });
+ 
+});
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+
+
+
+app.post('/api/exercise/add', (req, res) => {
+
+  findUser(req.userId, (err, data) => { 
+    if (err) return res.send('invalid userID');
+    return user = data;
+  });
+
+  createExercise(req.body, (err, data) => {
+    if (err) res.send('could not save the exercise');
+    return e = data;
+  });
+
+  res.send(user, e)  
+});
